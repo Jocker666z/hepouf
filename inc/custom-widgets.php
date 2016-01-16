8e7fa7d38cdc2631391f735ec8a6cbedd39ae1fe
jocker666z <r.barbarot@gmail.com>
2016-01-16 07:00:24 +0100

    yesterday miss add fix regression in widget-social-icons

--- inc/custom-widgets.php
+++ inc/custom-widgets.php
@@ -1,4 +1,32 @@
 <?php
+
+/**
+ * Helper function that updates fields in the dashboard form
+ *
+ * @since aThemes Widget Pack 1.0
+ */
+function hepouf_widgets_updated_field_value( $widget_field, $new_field_value ) {
+	extract( $widget_field );
+	
+	// Allow only integers in number fields
+	if( $hepouf_widgets_field_type == 'number' ) {
+		return absint( $new_field_value );
+		
+	// Allow some tags in textareas
+	} elseif( $hepouf_widgets_field_type == 'textarea' ) {
+		// Check if field array specifed allowed tags
+		if( !isset( $hepouf_widgets_allowed_tags ) ) {
+			// If not, fallback to default tags
+			$hepouf_widgets_allowed_tags = '<p><strong><em><a>';
+		}
+		return strip_tags( $new_field_value, $hepouf_widgets_allowed_tags );
+		
+	// No allowed tags for all other fields
+	} else {
+		return strip_tags( $new_field_value );
+	}
+}
+
 /**
  * Adds custom widgets
  */
